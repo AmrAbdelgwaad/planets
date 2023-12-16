aa61607e975d5b8f223fa8155699f5f54c11e725
AmrAbdelgwaad <115653887+AmrAbdelgwaad@users.noreply.github.com>
2023-12-16 17:43:40 +0200

    initial commit

---
+++ src/app/jupiter/jupiter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jupiter',
+  standalone: true,
+  imports: [],
+  templateUrl: './jupiter.component.html',
+  styleUrl: './jupiter.component.scss'
+})
+export class JupiterComponent {
+
+}
---
+++ src/app/uranus/uranus.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-uranus',
+  standalone: true,
+  imports: [],
+  templateUrl: './uranus.component.html',
+  styleUrl: './uranus.component.scss'
+})
+export class UranusComponent {
+
+}
