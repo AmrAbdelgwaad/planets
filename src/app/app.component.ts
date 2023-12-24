53ca30f1f4d3fc8b6f561bf9a4b3c0ab94ec3af0
AmrAbdelgwaad <115653887+AmrAbdelgwaad@users.noreply.github.com>
2023-12-24 12:32:01 +0200

    navigation  finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,36 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'planets';
+  anchors = [
+    {
+      name: 'mercury',
+      routerLink: '/mercury',
+      routerLinkActive: 'mercuryActive',
+    },
+    { name: 'venus', routerLink: '/venus', routerLinkActive: 'venusActive' },
+    { name: 'earth', routerLink: '/earth', routerLinkActive: 'earthActive' },
+    { name: 'mars', routerLink: '/mars', routerLinkActive: 'marsActive' },
+    {
+      name: 'jupiter',
+      routerLink: '/jupiter',
+      routerLinkActive: 'jupiterActive',
+    },
+    {
+      name: 'saturn',
+      routerLink: '/saturn',
+      routerLinkActive: 'saturnActive',
+    },
+    {
+      name: 'uranus',
+      routerLink: '/uranus',
+      routerLinkActive: 'uranusActive',
+    },
+    {
+      name: 'neptune',
+      routerLink: '/neptune',
+      routerLinkActive: 'neptuneActive',
+    },
+  ];
   state = false;
   toggle() {
     this.state = !this.state;
