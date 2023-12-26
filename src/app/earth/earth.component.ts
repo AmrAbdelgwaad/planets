b4f5b2b79c61f2ef0deea96c0281547a5994ad4e
AmrAbdelgwaad <115653887+AmrAbdelgwaad@users.noreply.github.com>
2023-12-26 12:55:30 +0200

    adding tabs

--- src/app/earth/earth.component.ts
+++ src/app/earth/earth.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-earth',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './earth.component.html',
-  styleUrl: './earth.component.scss'
+  styleUrl: './earth.component.scss',
 })
 export class EarthComponent {
+  tabs = [
+    {
+      id: 1,
+      label: 'Overview',
+      panelId: 'overview',
+    },
+    {
+      id: 2,
+      label: 'Internal',
+      panelId: 'internal',
+    },
+    {
+      id: 3,
+      label: 'Surface',
+      panelId: 'surface',
+    },
+  ];
+  activeTab = 1;
 
+  @HostListener('keydown', ['$event'])
+  handleKeyDown(event: KeyboardEvent): void {
+    switch (event.key) {
+      case 'ArrowRight':
+        this.activateNextTab();
+        break;
+      case 'ArrowLeft':
+        this.activatePreviousTab();
+        break;
+    }
+  }
+
+  activateTab(tabNumber: number): void {
+    this.activeTab = tabNumber;
+  }
+
+  activateNextTab(): void {
+    this.activeTab = (this.activeTab % 3) + 1;
+  }
+
+  activatePreviousTab(): void {
+    this.activeTab = this.activeTab === 1 ? 3 : this.activeTab - 1;
+  }
 }
